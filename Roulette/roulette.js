47a8f6bc10d808db3685756ad83f2fe56071fa5b
gbCzz <Eason0208@163.com>
2023-01-20 17:55:21 +0800

    完善文档注释

--- Roulette/roulette.js
+++ Roulette/roulette.js
@@ -1,7 +1,23 @@
 `use strict`;
 
+/**
+ * @typedef GameStatus
+ * @type {Object}
+ * @property {number} status 状态码
+ * @property {string} msg 状态信息
+ * @property {number | undefined} breech_left 剩余未开枪的枪膛数
+ * @property {number | undefined} bullet_left 剩余子弹数
+ */
+
 let game_data = {};
 
+/**
+ * 在指定群聊中开始一局轮盘赌
+ * @param {string | number} group_id 群号或其他能够唯一确定群聊的标识符
+ * @param {number} breech 左轮手枪的总膛数
+ * @param {number} bullets 枪膛中的子弹个数
+ * @returns {GameStatus} 执行状态
+ */
 function start(group_id, breech, bullets) {
 	if (typeof breech != 'number' || typeof bullets != 'number' || breech * bullets == 0)
 		return {
@@ -17,7 +33,7 @@ function start(group_id, breech, bullets) {
 			bullet_left: game_data[group_id].bullets,
 			breech_left: game_data[group_id].breech,
 			msg: `游戏开始，剩余 ${game_data[group_id].bullets} 枚，分布在 ${game_data[group_id].breech} 个后膛中`,
-		}
+		};
 	} else
 		return {
 			status: -1,
@@ -25,6 +41,11 @@ function start(group_id, breech, bullets) {
 		};
 }
 
+/**
+ * 在指定群聊的轮盘赌中开枪
+ * @param {number | string} group_id 群号或其他能够唯一确定群聊的标识符
+ * @returns {GameStatus} 执行状态
+ */
 function shoot(group_id) {
 	if (game_data?.[group_id]?.breech == undefined || game_data?.[group_id]?.breech == 0)
 		return {
@@ -68,3 +89,4 @@ module.exports = {
 	start,
 	shoot,
 };
+
